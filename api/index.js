bb862570d768a17d4389e004d9e5d197cdf1e1d3
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-08-01 15:21:02 +0700

    test vercel

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.get('/sse', (req, res) => {
   const interval = setInterval(() => {
     counter++;
     res.write(`data: ${counter}\n\n`);
-  }, 10000);
+  }, 1000);
 
   // จัดการการยุติการเชื่อมต่อของ client
   req.on('close', () => {

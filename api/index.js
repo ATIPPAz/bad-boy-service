ec87524e8ea0cc54cdec79199bc52b6cbd2ada93
atippaz <nasakun13201@gmail.com>
2023-08-04 15:15:47 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const bodyParser = require('body-parser')
 const app = express();
 app.use(cors());
+app.use(bodyParser.json())
+const roomData = []
+
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
 
 app.get('/pollingGetData', (req, res) => {
   const i = Math.floor(Math.random() * 2)
@@ -18,6 +34,23 @@ app.get('/pollingGetData', (req, res) => {
   }
   res.json(response)
 });
+
+app.post('/room',(req,res)=>{
+  if(req.body.roomName.trim() === ""){
+    req.body.roomName = makeid(5)
+  }
+  const id = uuidv4()
+  roomData.push({id,data:req.body})
+  res.json(id)
+})
+app.get('/room',(req,res)=>{
+  res.json( roomData)
+})
+
+app.get('/room/:roomId',(req,res)=>{
+  res.json(roomData.find(e=>e.id===req.params.roomId))
+})
+
 
 app.get('/', (req, res) => {
   res.json({welcome:'to my api'})

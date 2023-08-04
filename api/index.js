51e873d4b1e4e94e1e94ba0808b0342977217c58
atippaz <nasakun13201@gmail.com>
2023-08-04 17:48:04 +0700

    update mongo

--- api/index.js
+++ api/index.js
@@ -2,6 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://nasakun13201:pan28060@badminton.bkjs5n4.mongodb.net/', {
+  useNewUrlParser: true
+});
+const Schema = mongoose.Schema;
+
+const courtSchma = new Schema({
+  roomData:{
+    roomName:String,
+    allTeam: [{member: [String], order: Number}],
+    courtNumber: Number,
+    roomName: String,
+    teamLimit: Number,
+    winScore:Number,
+    winStreak:Number
+  } 
+});
+
+const Court = mongoose.model('Court',courtSchma);
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
@@ -34,32 +53,64 @@ app.get('/pollingGetData', (req, res) => {
   res.json(response)
 });
 
-app.post('/room',(req,res)=>{
+app.post('/room', async(req,res)=>{
   if(req.body.roomName.trim() === ""){
     req.body.roomName = makeid(5)
   }
-  const id = uuidv4()
-  roomData.push({id,data:req.body})
-  res.json({id,roomData})
+const roomData = {roomData:req.body}
+try{
+  const court = new Court(roomData);
+  await court.save().then(e=>{
+      res.json({id:e._id})
+  });
+}
+catch(e){
+  console.log(e);
+  res.json(500)
+}
+  
+  // roomData.push({id,data:req.body})
 })
-app.get('/room',(req,res)=>{
-  res.json( roomData)
+app.get('/room',async(req,res)=>{
+  try{
+    res.json( await Court.find())
+
+  }
+  catch(e){
+    console.log(e);
+    res.json(500)
+  }
 })
 
-app.get('/room/:roomId',(req,res)=>{
-  res.json(roomData.find(e=>e.id===req.params.roomId))
+app.get('/room/:roomId',async(req,res)=>{
+  try{
+    const result = await Court.findOne({_id: req.params.roomId })
+    res.json(result)
+  }
+  catch(e){
+    console.log(e);
+    res.json(500)
+  }
+  
 })
 
-app.delete('/room',(req,res)=>{
-  roomData.splice(0,roomData.length)
-  res.json(200)
+app.delete('/room',async(req,res)=>{
+  try{
+    roomData.splice(0,roomData.length)
+    await Court.DeleteMany(true)
+    res.json(200)
+  }
+  catch(e){
+    console.log(e);
+    res.json(500)
+  }
 })
 
 app.get('/', (req, res) => {
   res.json({welcome:'to my api'})
 });
 
-app.listen(3001, () => {
+app.listen(3001, async() => {
   console.log('Server is running on port 3000');
 });
 
